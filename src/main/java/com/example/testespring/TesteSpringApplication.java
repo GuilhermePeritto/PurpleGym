731e471613c76f0ff16d130131b8abfa6965b42a
GuilhermePeritto <perittoguilherme@gmail.com>
2023-10-10 20:58:27 -0300

    Funcionando com JAVAFX AMEMMMMMMMMMMMMMMMMMMM

--- src/main/java/com/example/testespring/TesteSpringApplication.java
+++ src/main/java/com/example/testespring/TesteSpringApplication.java
@@ -1,13 +1,43 @@
 package com.example.testespring;
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.net.URL;
+
 
 @SpringBootApplication
-public class TesteSpringApplication {
+public class TesteSpringApplication extends Application{
 
-	public static void main(String[] args) {
-		SpringApplication.run(TesteSpringApplication.class, args);
-	}
+    public ConfigurableApplicationContext springContext;
+    public Parent rootNode;
 
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(TesteSpringApplication.class);
+        URL fxmlLocation = getClass().getClassLoader().getResource("teste.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
+        fxmlLoader.setControllerFactory(springContext::getBean);
+        rootNode = fxmlLoader.load();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(rootNode));
+        stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        springContext.stop();
+    }
 }
